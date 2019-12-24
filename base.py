4969384e392d3e80f82477b360e09fb63ab71115
PhoeniXWhite <957360688@qq.com>
2019-12-24 20:53:21 +0800

    version 0.1

--- base.py
+++ base.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 
 # MongoDB的客户端
 client = MongoClient("localhost", 27017)
-db = client['tttt']
+db = client['test']
 # collection 相当于 table
 user_log_collection = db['user_log']
+
--- log_gui.py
+++ log_gui.py
@@ -1,5 +1,11 @@
+import time
 from PyQt5.Qt import *
 from resource.action_log import Ui_Form
+from ddbb import query_db
+from gen_csv import save_file
+
+HOUR_S = 3600
+DAY_H = 24
 
 
 class LogGui(QWidget, Ui_Form):
@@ -8,6 +14,9 @@ class LogGui(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        str_time = time.strftime('%Y-%m-%d', time.localtime())
+        self.st_le.setText(str_time)
+        self.ed_le.setText(str_time)
         self.st_le.get_focus_signel.connect(lambda: self.set_cur_obj(self.st_le))
         self.ed_le.get_focus_signel.connect(lambda: self.set_cur_obj(self.ed_le))
 
@@ -22,7 +31,25 @@ class LogGui(QWidget, Ui_Form):
         self.current_le = cur_obj
 
     def gen_csv(self):
-        print('generated')
+        start_time = self.st_le.text()
+        end_time = self.ed_le.text()
+        pd_no = self.pd_le.text()
+
+        if start_time:
+            start_time = time.mktime(time.strptime(start_time, '%Y-%m-%d'))
+        if end_time:
+            end_time = time.mktime(time.strptime(end_time, '%Y-%m-%d')) + HOUR_S * DAY_H
+        if start_time > end_time:
+            QMessageBox.information(self, 'Tips', 'wrong time information', QMessageBox.Ok)
+
+        res = query_db(start_time, end_time, pd_no)
+        # [print(r) for r in res]
+        if not res:
+            QMessageBox.information(self, '', 'Nobody operated during this period', QMessageBox.Ok)
+            return
+        res = save_file(res)
+        if res:
+            QMessageBox.information(self, '', 'generate successful', QMessageBox.Ok)
 
 
 if __name__ == '__main__':
